2800c452d07e17055f4157b2f89bc8d07921032a
Konstantinos Papakonstantinou <kpapakonstantinou@idirect.net>
2023-11-30 09:31:23 +0100

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
-const app = express()
-
-app.listen(3000, () => console.log('Server listening on port 3000'))
+dotenv.config()
+mongoose
+  .connect(process.env.MONGODB_CONN)
+  .then(() => {
+    const app = express()
+    app.listen(3000, () => console.log('Server listening on port 3000'))
+  })
+  .catch((error) => console.log(error))
