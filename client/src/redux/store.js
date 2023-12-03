ec78ec5a2fdcaab57b53964bab756b36406c1db7
Konstantinos Papakonstantinou <kpapakonstantinou@idirect.net>
2023-12-03 15:42:15 +0100

    persist user with redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice.js'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    user: userReducer,
+  })
+)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 })
+
+export const persistedStore = persistStore(store)
